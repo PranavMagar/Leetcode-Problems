343f38652fe38dda022e723d203a079d0a0b0cee
Pranav <pranav.magar5001@gmail.com>
2025-09-15 23:37:18 +0530

    9

--- Palindrome.py
+++ Palindrome.py
@@ -1,5 +1,5 @@
 class Solution:
-    def isPalindrome (self,s:str)->bool:
+    def isPalindrome (self,x: int)->bool:
         if x<0:
             return False
         return str(x) == str(x)[::-1]
