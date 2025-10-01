fe554c50c25da04187a49d772a54bb24e29c5bce
Pranav <pranav.magar5001@gmail.com>
2025-10-01 11:43:55 +0530

    137

---
+++ SingleNumber2.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def singlenNumber(self,nums: List[int])->int:
+        stack = []
+        stack2 = []
+        count = Counter(nums)
+        for i in count:
+            if count[i]>1:
+                stack.append(i)
+            else:
+                stack2.append(i)
+        for i in stack2:
+            return i
